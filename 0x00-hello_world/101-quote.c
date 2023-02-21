a4afd97c5e5f7bc875585ac8ac069226e821871e
Yohannes3792 <121262936+Yohannes3792@users.noreply.github.com>
2023-02-21 23:10:38 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<unistd.h>
 /**
-* main - Entry point
-*
-* Return: Always 1 (Success)
-*/
+ * main - program that prints to the standard error
+ *
+ * Return: 1
+ */
 int main(void)
 {
-        write(2,
-        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
-}
+fput("that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
