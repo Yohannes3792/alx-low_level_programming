2d7b96e74f8ff2900b7eb8733453372dad453c4f
Joni084 <yohannesmulugeta084@gmail.com>
2023-02-22 01:05:42 +0300

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,21 @@
-#include<stdio.h>
-#include<unistd.h>
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <unistd.h>
+
 /**
- * main - program that prints to the standard error
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- * Return: 1
+ * Return: Always 1.
  */
 int main(void)
 {
-fput("that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - c program that prints using
- * printf
+ * main - Entry point
  *
- * Return: 0
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
